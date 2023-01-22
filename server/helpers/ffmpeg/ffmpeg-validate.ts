c5415b088e4413bb8098e57df87ea8e86a1ac986
kontrollanten <6680299+kontrollanten@users.noreply.github.com>
2023-01-22 05:58:40 +0100

    feat(transcoding): validare video file

---
+++ server/helpers/ffmpeg/ffmpeg-validate.ts
@@ -0,0 +1,33 @@
+import ffmpeg from 'fluent-ffmpeg'
+import { Job } from 'bullmq'
+import { lTags } from '@server/lib/object-storage/shared'
+import { logger } from '../logger'
+import { runCommand } from './ffmpeg-commons'
+
+async function validateVideoFile (options: {
+  job?: Job
+  path: string
+}) {
+  logger.debug('Will validate video file.', { options, ...lTags() })
+
+  let stderr = ''
+  const command = ffmpeg(options.path)
+    .addOption('-max_muxing_queue_size', '1024')
+    .addOption('-loglevel', 'error')
+    .addOption('-f', 'null')
+    .output('/dev/null')
+    .on('stderr', (s) => {
+      stderr += s
+      return command.kill('SIGKILL')
+    })
+
+  try {
+    await runCommand({ command, job: options.job })
+  } catch (err) {
+    throw Error(`Video validation failed for file ${options.path}: ${stderr}`)
+  }
+}
+
+export {
+  validateVideoFile
+}
