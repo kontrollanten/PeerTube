f462dbe429ea93443a04c68e6b2f6a1b82fc87ba
ar9708 <ar9708@users.noreply.github.com>
2023-04-29 20:36:12 +0000

    Moar forgiving workaround, please pass tests

--- packages/server-commands/src/server/config-command.ts
+++ packages/server-commands/src/server/config-command.ts
@@ -467,7 +467,7 @@ export class ConfigCommand extends AbstractCommand {
       },
       live: {
         enabled: true,
-        useObjectStorage: false,
+        useObjectStorage: true,
         allowReplay: false,
         latencySetting: {
           enabled: false
